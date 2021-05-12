e654a801032c168830f991444daa21b5fd9381ba
Simon Lundström <github-commits@soy.se>
2021-05-12 17:59:39 +0200

    Start using click and pika

--- pinboard_archiver.py
+++ pinboard_archiver.py
@@ -1,9 +1,52 @@
 __version__ = "0.1.0"
 
 
-def main():
-    print("lol")
+import logging
+import typing
+
+import click
+import pika  # type: ignore
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    main()
+def callback(ch, method, properties, body):
+    log.info(" [x] Received %r" % body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+@click.command()
+@click.option(
+    "--amqp-url",
+    required=True,
+    show_envvar=True,
+    help="URL to AMQP server",
+)
+def main(*, amqp_url: str):
+    """An archiving consumer for Pinboard.in Message Queue"""
+    connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
+    channel = connection.channel()
+    channel.basic_qos(prefetch_count=1)
+    pinboard_archive = channel.queue_declare(
+        queue="pinboard.archive",
+        durable=True,
+        auto_delete=False,
+        arguments={"x-queue-mode": "lazy"},
+    )
+    channel.queue_bind(
+        exchange="pinboard",
+        routing_key="pinboard.*",
+        queue=pinboard_archive.method.queue,
+    )
+    channel.basic_consume(
+        queue=pinboard_archive.method.queue,
+        auto_ack=False,
+        on_message_callback=callback,
+    )
+    log.info(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+    connection.close()
+
+
+main(auto_envvar_prefix="PINQUE")
