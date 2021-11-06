6100ebf4f922b7c03a70038fbbef883a9bd83d88
Simon Lundström <github-commits@soy.se>
2021-11-06 14:19:32 +0100

    Add httpbin as a "archiver"

--- pinboard_archiver.py
+++ pinboard_archiver.py
@@ -6,13 +6,16 @@ except ModuleNotFoundError:
 import logging
 import os
 import typing
+import urllib.parse
+import urllib.request
 
 import capnp  # type: ignore
 import click
 import pika  # type: ignore
 
 package_name = __spec__.name  # type: ignore
-__version__ = importlib_metadata.version(package_name)
+__metadata__ = importlib_metadata.metadata(package_name)
+__version__ = __metadata__["Version"]
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(package_name)
@@ -21,7 +24,7 @@ pinboard_post_schema = os.path.dirname(__file__) + "/pinboard_post.capnp"
 pinboard_post = capnp.load(pinboard_post_schema)
 
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body, opener):
     log.info(
         "Received message",
         extra={"message_id": properties.message_id},
@@ -30,8 +33,16 @@ def callback(ch, method, properties, body):
 
     post = pinboard_post.PinboardPost.from_bytes(body)
     log.debug("Deserialized message", extra={"post": body})
+    httpbin(opener=opener, url=post.href)
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def httpbin(*, opener, url):
+    data = urllib.parse.urlencode({"url": url})
+    data = data.encode("ascii")
+    with opener.open("https://httpbin.org/status/429", data=data, timeout=5) as file:
+        log.info(file.read().decode("utf-8"))
 
 
 @click.version_option()
@@ -44,6 +55,18 @@ def callback(ch, method, properties, body):
 )
 def main(*, amqp_url: str):
     """An archiving consumer for Pinboard.in Message Queue"""
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        (
+            "User-agent",
+            "{}/{} (+{})".format(
+                __metadata__["Name"],
+                __metadata__["Version"],
+                __metadata__["Home-page"],
+            ),
+        )
+    ]
+
     connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
     channel = connection.channel()
     channel.basic_qos(prefetch_count=1)
@@ -61,7 +84,9 @@ def main(*, amqp_url: str):
     channel.basic_consume(
         queue=pinboard_archive.method.queue,
         auto_ack=False,
-        on_message_callback=callback,
+        on_message_callback=lambda ch, method, properties, body: callback(
+            ch, method, properties, body, opener
+        ),
     )
     log.info(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
